2b417eba1bf20ca839f3a8f80b455eaae7e05d48
Fernando <fernando.luiz92@hotmail.com>
2021-11-22 21:51:02 -0300

    estudos sobre stream

---
+++ src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program {
+    public static void main(String[]args){
+
+        List<Integer> list = Arrays.asList(3,4,5,10,7);
+
+        //Criar uma stream a partir de uma coleção
+        Stream<Integer> st1 = list.stream().map(x -> x * 10);
+
+        System.out.println(Arrays.toString(st1.toArray()));
+
+
+        //Criar uma stream a partir do Stream of
+        Stream<String> st2 = Stream.of("Fernando","Lauren","Mae");
+        System.out.println(Arrays.toString(st2.toArray()));
+
+        //Criar uma stream a partir do Stream.iterate
+        Stream<Integer> st3 = Stream.iterate(0,x -> x+2);
+        System.out.println(Arrays.toString(st3.limit(10).toArray()));
+
+        //Sequencia de fibbonacci
+        Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p-> new Long[] {p[1], p[0]+p[1]}).map(p->p[0]);
+        System.out.println(Arrays.toString(st4.limit(10).toArray()));
+
+
+
+        System.out.println();
+    }
+}
